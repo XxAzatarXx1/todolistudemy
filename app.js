5f8f52fcc313d2323402de3f47a6a32681eea998
XxAzatarXx1 <azatargaming@gmail.com>
2026-04-19 16:23:12 -0400

    feat: initialize todos from localStorage and update addTodo functionality

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const addTodoBtn = document.getElementById("addTodoBtn")
 const inputTag = document.getElementById("todoInput")
 let todoText; // This should be populated when the user clicks on Add button
+
 let todos = [];
+let todosString = localStorage.getItem("todos")
+// If we have todos in the localStorage, we will read it
+if(todosString) {
+    todos = JSON.parse(todosString)
+}
+
 
 addTodoBtn.addEventListener("click", () => {
     console.log("Hey I just clicked")
     todoText = inputTag.value 
     console.log(todoText)
     inputTag.value = ""
+    let todo = {
+        title: todoText,
+        isCompleted: false
+    }
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos))
 });
